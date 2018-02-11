1b8295952859dc56335241aadb21313d26c4328a
Charles Calder <cc9431@bard.edu>
2018-02-10 23:39:10 -0500

    Comments

--- Canopy/spiders/store_spider.py
+++ Canopy/spiders/store_spider.py
@@ -1,54 +1,55 @@
 '''
 Uses Scrapy to gather information from https://www.apple.com/retail/storelist/.
 Feilds: Country, State/Region, City, Store Name, Address, Picture URL
-Outputs json file containing information of all apple stores.
+Outputs json file containing information of all 501 apple stores.
 
-Charles Calder
-February 10th, 2018
+Developer: Charles Calder
+Start Date: February 10th, 2018
 
 NOTE: This script runs on python 3.6
 '''
 
-#import scrapy_splash
 import scrapy
 from Canopy.items import AppleItem
 
 class AppleStoreSpider(scrapy.Spider):
 	'''Basic spider class'''
+	# Used for calling spider with scrapy
 	name = 'apple'
+	# Domain in which spider is allowed to move
 	allowed_domains = ['apple.com']
+	# Url to be joined with extracted urls
 	base_url = 'https://www.apple.com'
+	# Url the spider starts with
 	start_urls = ['https://www.apple.com/retail/storelist/']
 
 	def parse(self, response):
 		# Gather and organize response data from website
-		countries = response.xpath("//div[contains(@id, 'stores')]")
+		countries = response.xpath("//div[contains(@id, 'stores')]") # List of country sections
 		for country in countries:
-			sites = country.xpath('*//a/@href').extract()
+			sites = country.xpath('*//a/@href').extract() # List of domains to be joined with base
 			for site in sites:
 				if site is not None:
+					# If the new site exists, analyze the store page with a store specific method
 					url = str.format("{}{}", self.base_url, site)
 					yield scrapy.Request(url, callback=self.parse_store_page)
 
 	def parse_store_page(self, response):
+		# Sort data for store pages into AppleItem
 		def css_extract(section, text, key):
+			# Turn a few values into an item key-value pair (for readability)
 			query = str.format("//{}[contains(@class, '{}')]/text()", section, text)
 			string = response.xpath(query).extract_first()
+			# This allows us to only see non-null key-value pairs
 			if string is not None:
 				item[key] = string.strip()
 
 		item = AppleItem()
-		css_extract("h1", "headline", "name")
-		css_extract("span", "store-region", "region")
-		css_extract("span", "store-locality", "city")
-		css_extract("span", "store-street", "address")
-		css_extract("a", "ac-gf-footer-locale-link", "country")
-		#item['img'] = working on it :)
-		
-		# postal_code = css_extract("span", "store-postal-code", )
-		# if postal_code is not None:
-		# 	if item['region'] is None:
-		# 		item['region'] = postal_code
-		# 	else:
-		# 		item['region'] = str.format("{}, {}", item['region'], postal_code)
+		css_extract("span", "store-street", "address")			# Street address
+		css_extract("h1", "headline", "name")					# Name of store
+		css_extract("span", "store-region", "region")			# Region/State
+		css_extract("a", "ac-gf-footer-locale-link", "country")	# Country
+		css_extract("span", "store-locality", "city")			# City
+		css_extract("" , "", "image") # item['img'] = working on it :)
+
 		yield item
--- README.md
+++ README.md
@@ -4,9 +4,10 @@ Charles Calder
 
 February 10th, 2018
 
-The store_spider.py file runs on python 3.6, and uses Scrapy 1.5.0. This script will scrape information from the webpage "https://www.apple.com/retail/storelist/" and return a json file containing relevant information on each Apple store.
+The store_spider.py file runs on python 3.6, and uses Scrapy 1.5.0. This script will scrape information from the webpage "https://www.apple.com/retail/storelist/" and return a json file containing relevant information on all 501 Apple stores.
 
 todo 
 
+* Comments
 * Translation issues
 * Postal code
